e9bdf15b7bd8b5e9847d35cc8fabe25b1bb050fc
Luigi Robles <luigi.robles33@gmail.com>
2020-07-20 22:26:29 +0000

    status check

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Intermediate19.cpp - displays the account balance at
+ //Intermediate19.cpp - displays the account balance at
 //the end of 1 through 3 years
 //Created/revised by <your name> on <current date>
 
@@ -8,35 +8,43 @@
 using namespace std;
 
 //function prototype
-double getBalance(int amount, double rate, int y);
+void getBalance(int amount, 
+                double &rate, 
+                int &y);
+void displayBalance();
 
 int main()
 {
 	int deposit = 0;
-	double interestRate = 0.0;
 	double acctBalance = 0.0;
+    double lowestRate = 0.0;
+    double highestRate = 0.0;
 
 	cout << "Deposit: ";
 	cin >> deposit;
-	cout << "Rate (in decimal form): ";
-	cin >> interestRate;
-
+	cout << "Lowest Rate (in decimal form): ";
+	cin >> lowestRate;
+    cout << "Highest Rate (in decimal form): " ;
+    cin >> highestRate;
+    
 	cout << fixed << setprecision(2);
 	for (int year = 1; year < 4; year += 1)
 	{
-		acctBalance = 
-			getBalance(deposit, interestRate, year);
+			getBalance(deposit,lowestRate, year);
 		cout << "Year " << year << ": $" 
 			<< acctBalance  << endl;
 	}  //end for
-
-	return 0;
+    return 0;
 }   //end of main function
 
 //*****function definitions*****
-double getBalance(int amount, double rate, int y)
+void getBalance(int amount, double &rate, int &y)
 {
 	double balance = 0.0;
 	balance = amount * pow((1 + rate), y);
-	return balance;
 }  //end of getBalance function
+
+void displayBalance()
+{
+    cout << " "
+}
